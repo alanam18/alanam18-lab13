ba59fe7e6a3c0dbaf59e40ede16e3e3a5ece8365
alanam18 <alanam1@gwmail.gwu.edu>
2026-04-21 23:18:34 -0400

    Updated Server added the class and constructor

--- Server.java
+++ Server.java
@@ -1,1 +1,14 @@
+import java.net.*;
+import java.io.*;
+import java.time.*;
+import java.util.*;
+import java.util.concurrent.*;
 
+public class Server {
+    private ServerSocket serverSocket;
+    private List<LocalDateTime> connectedTimes = Collections.synchronizedList(new ArrayList<>());
+
+    public Server(int port) throws Exception {
+        serverSocket = new ServerSocket(port);
+    }
+  
