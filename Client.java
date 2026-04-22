b33a18a12d45b9f36e7a3dd41b2ca8518292f043
alanam18 <alanam1@gwmail.gwu.edu>
2026-04-21 22:05:08 -0400

    Started Client by creating the constructor and class

--- Client.java
+++ Client.java
@@ -1,1 +1,13 @@
+import java.net.*;
+import java.io.*;
 
+public class Client {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+  
+    public Client(String host, int port) throws Exception {
+        socket = new Socket(host, port);
+        out = new PrintWriter(socket.getOutputStream());
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
