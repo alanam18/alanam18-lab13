e3c1eb9f43eb799d2be3c9a6891514f378e8d72c
alanam18 <alanam1@gwmail.gwu.edu>
2026-04-21 22:52:48 -0400

    Completed Client by adding in everything it needs; the request,disconnect,getSocket, and handshake

--- Client.java
+++ Client.java
@@ -11,3 +11,25 @@ public class Client {
         out = new PrintWriter(socket.getOutputStream());
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void handshake() throws Exception {
+        out.println("12345");
+        out.flush();
+    }
+
+    public String request(String number) throws Exception {
+        out.println(number);
+        out.flush();
+        return in.readLine();
+    }
+
+    public void disconnect() throws Exception {
+        in.close();
+        out.close();
+        socket.close();
+    }
+}
